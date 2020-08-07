632f668eab491737c7bbf0dc59864575ebca2a5e
fabricio0921 <fabricioestudo@gmail.com>
2020-08-06 23:03:06 -0300

    implementacao contructor

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,6 +5,23 @@ import { createStackNavigator } from 'react-navigation-stack';
 import FormRow from '../components/FormRow';
 
 export default class LoginPage extends React.Component {
+    constructor(props){
+        super(props)
+            this.state={
+
+            mail:'',
+            password:''
+        }
+    }
+
+    onChangeHandler(field, value){
+        this.setState({
+            [field]:value
+        })
+
+    }
+
+
     render() {
         return (
             <View >
@@ -12,11 +29,16 @@ export default class LoginPage extends React.Component {
                     <TextInput
                         style={styles.input}
                         placeholder="Email"
+                        value={this.state.mail}
+                        onChangeText={value =>this.onChangeHandler('mail',value)}
                     />
                 </FormRow>
                 <FormRow>
-                    <TextInput placeholder="Senha"
-                        secureTextEntry
+                    <TextInput 
+                    placeholder="Senha"
+                    secureTextEntry
+                    value={this.state.password}
+                    onChangeText={value => this.onChangeHandler('password',value)}
                     />
                 </FormRow>
             </View>
